54a46bb5bbdfaa653bce1f704d40ef1d6d235c0f
Tobias Jansson <tobias.jansson@gmail.com>
2017-11-23 22:41:39 +0100

    devices and calendar endpoints working

--- hemmaServer.js
+++ hemmaServer.js
@@ -6,45 +6,73 @@ const exec = require('child_process').exec;
 const express = require('express');
 const port = 3001;
 
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
+const TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
+
 var app = express(),
     server = require('http').createServer(app);
 
-app.all('/*', function(req, res, next) {
+app.all('/*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     next();
 });
 
-app.get('/device/', function(req, res) {
+app.get('/device/', function (req, res) {
     console.log("devices");
-    // Need to use promises 
-    res.send(listDevices());
-});  
+    listDevices().then((data) => {
+        res.send(data);
+    });
+});
+
+app.get('/calendar/', function (req, res) {
+    console.log("calendar");
+    getEventsFromCalendar().then((data) => {
+        res.send(data);
+    });
+});
 
 server.listen(port);
 var ts = (new Date()).toLocaleString() + "  ";
 
-console.log (ts + 'Running on port ' + port);
+console.log(ts + 'Running on port ' + port);
 
+function getEventsFromCalendar() {
+    return new Promise((resolve, reject) => {
+        // Load client secrets from a local file.
+        fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+            if (err) {
+                console.log('Error loading client secret file: ' + err);
+                reject(err);
+            }
+            //
+            // Authorize a client with the loaded credentials, then call the
+            // Google Calendar API.
+            authorize(JSON.parse(content)).then((auth) => {
+                listEvents(auth).then((data) => {
+                    resolve(data);
+                });
+            });
+        });
+    });
+}
 
-// If modifying these scopes, delete your previously saved credentials
-// at ~/.credentials/calendar-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
-var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
-    process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
-
-// Load client secrets from a local file.
-fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-  // Authorize a client with the loaded credentials, then call the
-  // Google Calendar API.
-  authorize(JSON.parse(content), listEvents);
-
-//  listDevices();
-});
+function authorize(credentials) {
+    var clientSecret = credentials.installed.client_secret;
+    var clientId = credentials.installed.client_id;  
+    var auth = new googleAuth();
+    var oauth2Client = new auth.OAuth2(clientId, clientSecret, '');
+    return new Promise((resolve,reject) => {
+        fs.readFile(TOKEN_PATH, function (err, token) {
+            if (err) {
+                reject(err);
+            } else {
+                oauth2Client.credentials = JSON.parse(token);
+                resolve(oauth2Client);
+            }
+        }); 
+    });
+}
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -53,22 +81,23 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var auth = new googleAuth();
-  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+function authorize2(credentials, callback) {
+    var clientSecret = credentials.installed.client_secret;
+    var clientId = credentials.installed.client_id;
+    var redirectUrl = credentials.installed.redirect_uris[0];
+    var auth = new googleAuth();
+    var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, callback);
-    } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client);
-    }
-  });
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function (err, token) {
+        if (err) {
+            getNewToken(oauth2Client, callback);
+            return;
+        } else {
+            oauth2Client.credentials = JSON.parse(token);
+            return callback(oauth2Client);
+        }
+    });
 }
 
 /**
@@ -80,27 +109,27 @@ function authorize(credentials, callback) {
  *     client.
  */
 function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
-      }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
+    var authUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES
     });
-  });
+    console.log('Authorize this app by visiting this url: ', authUrl);
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Enter the code from that page here: ', function (code) {
+        rl.close();
+        oauth2Client.getToken(code, function (err, token) {
+            if (err) {
+                console.log('Error while trying to retrieve access token', err);
+                return;
+            }
+            oauth2Client.credentials = token;
+            storeToken(token);
+            callback(oauth2Client);
+        });
+    });
 }
 
 /**
@@ -109,15 +138,15 @@ function getNewToken(oauth2Client, callback) {
  * @param {Object} token The token to store to disk.
  */
 function storeToken(token) {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
+    try {
+        fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+        if (err.code != 'EEXIST') {
+            throw err;
+        }
     }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-  console.log('Token stored to ' + TOKEN_PATH);
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+    console.log('Token stored to ' + TOKEN_PATH);
 }
 
 /**
@@ -126,53 +155,54 @@ function storeToken(token) {
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
 function listEvents(auth) {
-  var calendar = google.calendar('v3');
-  calendar.events.list({
-    auth: auth,
-    calendarId: '8d9vj753tdtto51s74ddbvlg3o@group.calendar.google.com',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime'
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    }
-    var events = response.items;
-    if (events.length == 0) {
-      console.log('No upcoming events found.');
-    } else {
-      console.log('Upcoming 10 events:');
-      for (var i = 0; i < events.length; i++) {
-        var event = events[i];
-        var start = event.start.dateTime || event.start.date;
-        console.log('%s - %s', start, event.summary);
-      }
-    }
-  });
+    return new Promise((resolve, reject) => {
+        var calendar = google.calendar('v3');
+        calendar.events.list({
+            auth: auth,
+            calendarId: '8d9vj753tdtto51s74ddbvlg3o@group.calendar.google.com',
+            timeMin: (new Date()).toISOString(),
+            maxResults: 10,
+            singleEvents: true,
+            orderBy: 'startTime'
+        }, function (err, response) {
+            if (err) {
+                console.log('The API returned an error: ' + err);
+                reject(err);
+            }
+            var events = response.items;
+            if (events.length == 0) {
+                console.log('No upcoming events found.');
+                resolve([]);
+            } else {
+                resolve(events);
+            }
+        });
+        console.log('Events');
+    });
 }
 
 function listDevices() {
-    exec('tdtool --list', (err, stdout, stderr) => {
-        if(err) {
-            console.error(err);
-            return;
-        }
-        var lines = stdout.split('\n');
-        var devices = [];
-        for(var i=0;i<lines.length;i++) {
-            var line = lines[i].split('\t');
-            //console.log(line);
-            if(line.length > 1) {
-                var device = { name: line[1],
-                               id: line[2],
-                               state: line[2]};
-                devices.push(device);
+    return new Promise((resolve, reject) => {
+        exec('tdtool --list', (err, stdout, stderr) => {
+            if (err) {
+                console.error(err);
+                reject(err);
             }
-        }
-        console.log(devices);
-        return devices;
+            var lines = stdout.split('\n');
+            var devices = [];
+            for (var i = 0; i < lines.length; i++) {
+                var line = lines[i].split('\t');
+                if (line.length > 1) {
+                    var device = {
+                        name: line[1],
+                        id: line[2],
+                        state: line[2]
+                    };
+                    devices.push(device);
+                }
+            }
+            console.log(devices);
+            resolve(devices);
+        });
     });
-    return [];
 }
